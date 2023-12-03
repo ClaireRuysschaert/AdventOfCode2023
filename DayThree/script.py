6df1ee2356a5fecbe586e1c60ac9e92b5a537a74
ClaireRuysschaert <netclaire@hotmail.fr>
2023-12-03 15:10:20 +0100

    feat: multiply numbers adjacent by *

--- DayThree/script.py
+++ DayThree/script.py
@@ -1,6 +1,6 @@
 import string
 
-with open("AdventOfCode2023/DayThree/input.txt", "r") as f:
+with open("AdventOfCode2023/DayThree/test.txt", "r") as f:
     lines = []
     for line in f:
         lines.append(line)
@@ -33,7 +33,7 @@ def get_numbers_from_line(line):
                 complete_numbers_positions.append(
                     (complete_number, start_position, end_position)
                 )
-        elif char != "." and char in string.punctuation:
+        elif char == "*" and char in string.punctuation:
             symbols_positions.append((char, start_position-1, start_position+1))
             char_position += 1
         else:
@@ -49,25 +49,30 @@ def compare_lines(prev_line, curr_line, next_line):
     next_numbers, next_symbols = (
         get_numbers_from_line(next_line) if next_line else ([], [])
     )
-    sum_numbers = 0
+    
+    gear_numbers = 0
     for number, start, end in curr_numbers:
         symbol_in_prev_line = any(sym_start <= start <= sym_end or sym_start <= end <= sym_end for symbol, sym_start, sym_end in prev_symbols)
         symbol_in_next_line = any(sym_start <= start <= sym_end or sym_start <= end <= sym_end for symbol, sym_start, sym_end in next_symbols)
         symbol_next_to_number = any(sym_end == start or sym_start == end for symbol, sym_start, sym_end in curr_symbols)
-        if symbol_in_prev_line or symbol_in_next_line or symbol_next_to_number:
-            print(f"For number {number}, yes")
-            sum_numbers += int(number)
+        adjacent_numbers = [num for num, num_start, num_end in curr_numbers if num_start == end + 1 or num_end == start - 1]
+        if symbol_in_prev_line or symbol_in_next_line or symbol_next_to_number and len(adjacent_numbers) == 2:
+            # print(f"For number {number}, yes")
+            gear_numbers = number
         else:
             pass
-            print(f"For number {number}, no")
-    # print(f"Sum numbers: {sum_numbers}")
-    return sum_numbers
+            # print(f"For number {number}, no")
+    return gear_numbers
 
+gear_numbers = []
 total_sum = 0
 for i in range(len(lines)):
     prev_line = lines[i - 1] if i - 1 >= 0 else None
     next_line = lines[i + 1] if i + 1 < len(lines) else None
-    total_sum += compare_lines(prev_line, lines[i], next_line)
-    print(f"line {i} done, total sum {total_sum}")
+    if compare_lines(prev_line, lines[i], next_line) != 0:
+        gear_numbers.append(int(compare_lines(prev_line, lines[i], next_line)))
+
+gear_numbers = [gear_numbers[i] * gear_numbers[i + 1] for i in range(0, len(gear_numbers), 2)]
+total_sum = sum(gear_numbers)
 
 print(f"Total sum: {total_sum}")
